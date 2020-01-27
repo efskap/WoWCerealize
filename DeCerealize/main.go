831afe313caf8e1d9c817f2ccb923ac31a96efba
Dmitry Narkevich <me@dmitry.lol>
2020-01-27 15:40:38 +0200

    better loop timing & go fmt

--- DeCerealize/main.go
+++ DeCerealize/main.go
@@ -10,28 +10,32 @@ import (
 
 const CerealPixels = 64 // must match value in addon lua
 const DisplayNumber = 0 // which display you have WoW open on (in fullscreen!)
+const Interval = 100 * time.Millisecond
 
 func main() {
 	DC := DeCerealizer{}
-	stringChan := make(chan  string, 64)
-	defer close (stringChan)
+	stringChan := make(chan string, 64)
+	defer close(stringChan)
 	go DC.Start(stringChan)
+	// just print new messages as they come in
 	for {
-		str := <- stringChan
+		str := <-stringChan
 		fmt.Println(str)
 	}
 }
+
 type DeCerealizer struct {
-	buf bytes.Buffer
+	buf            bytes.Buffer
 	expectedMsgNum int
 }
 
-func (d *DeCerealizer) Start(out chan <- string) {
-	// align loop with system clock on the second
-	timeUntilSecond := time.Until(time.Now().Add(time.Second).Truncate(time.Second))
-	time.Sleep(timeUntilSecond)
+func (d *DeCerealizer) Start(out chan<- string) {
 	for {
-		time.Sleep(100*time.Millisecond)
+		// align loop with system clock on the interval
+		// so with a 100ms interval the snapshot will happen at Xs:0ms, Xs:100ms, Xs:200ms, etc
+		// (unless it takes longer than the interval, in which case it'll skip a beat)
+		timeUntilAlign := time.Until(time.Now().Add(Interval).Truncate(Interval))
+		time.Sleep(timeUntilAlign)
 		msgNum, msgBytes, err := d.getNextMsg()
 		if _, ok := err.(*ChecksumError); ok {
 			//fmt.Println(e.Error())
@@ -40,12 +44,12 @@ func (d *DeCerealizer) Start(out chan <- string) {
 			fmt.Println("error: ", err.Error())
 			continue
 		}
-		if msgNum == d.expectedMsgNum -1  {
+		if msgNum == d.expectedMsgNum-1 {
 			// message has not updated yet
 			continue
 		} else {
 			// TODO: perhaps warn when badly out of sync
-			d.expectedMsgNum= msgNum
+			d.expectedMsgNum = msgNum
 		}
 		for _, b := range msgBytes {
 			if b == '\n' {
@@ -57,56 +61,57 @@ func (d *DeCerealizer) Start(out chan <- string) {
 				d.buf.WriteByte(b)
 			}
 		}
-		d.expectedMsgNum ++
+		d.expectedMsgNum++
 	}
 }
 func (d *DeCerealizer) getNextMsg() (msgNum int, msgBytes []byte, err error) {
+	displayBounds := screenshot.GetDisplayBounds(DisplayNumber)
+	// bottom right strip of displayBounds
+	bounds := image.Rectangle{
+		Min: displayBounds.Max.Sub(image.Point{
+			X: CerealPixels,
+			Y: 1,
+		}),
+		Max: displayBounds.Max,
+	}
 
-		displayBounds := screenshot.GetDisplayBounds(DisplayNumber)
-		// bottom right strip of displayBounds
-		bounds := image.Rectangle{
-			Min: displayBounds.Max.Sub(image.Point{
-				X: CerealPixels,
-				Y: 1,
-			}),
-			Max: displayBounds.Max,
-		}
+	img, err := screenshot.CaptureRect(bounds)
+	if err != nil {
+		return 0, nil, fmt.Errorf("error capturing screen: %w", err)
+	}
+	var imgBytes [CerealPixels * 3]byte
+	for x := 0; x < CerealPixels; x++ {
+		col := img.RGBAAt(x, 0)
+		imgBytes[x*3] = col.R
+		imgBytes[x*3+1] = col.G
+		imgBytes[x*3+2] = col.B
+	}
+	msgNum = int(imgBytes[0])
+	remoteChecksum := int(imgBytes[1])
+	msg := imgBytes[2:]
 
+	localChecksum := 0
+	for _, b := range msg {
+		localChecksum += int(b)
+	}
+	localChecksum = localChecksum & 0xFF
 
-		img, err := screenshot.CaptureRect(bounds)
-		if err != nil {
-            return 0, nil, fmt.Errorf("error capturing screen: %w", err)
+	if remoteChecksum != localChecksum {
+		return 0, nil, &ChecksumError{
+			expected: remoteChecksum,
+			got:      localChecksum,
+			buffer:   imgBytes[:],
 		}
-		var imgBytes [CerealPixels *3]byte
-		for x := 0 ; x < CerealPixels; x++ {
-			col := img.RGBAAt(x, 0)
-			imgBytes[x*3] = col.R
-			imgBytes[x*3 + 1] = col.G
-			imgBytes[x*3 + 2] = col.B
-		}
-		msgNum = int(imgBytes[0])
-		remoteChecksum := int(imgBytes[1])
-		msg := imgBytes[2:]
-
-		localChecksum := 0
-		for _, b := range msg {
-			localChecksum += int(b)
-		}
-		localChecksum = localChecksum & 0xFF
-
-		if remoteChecksum != localChecksum {
-			return 0, nil , &ChecksumError{
-				expected: remoteChecksum,
-				got:      localChecksum,
-				buffer:   imgBytes[:],
-			}
-		}
-		return msgNum, msg, nil
+	}
+	return msgNum, msg, nil
 }
 
 type ChecksumError struct {
 	expected int
-	got int
-	buffer []byte
+	got      int
+	buffer   []byte
 }
-func (e *ChecksumError) Error() string { return fmt.Sprintf("checksum error (%d != expected %d)", e.got, e.expected)}
+
+func (e *ChecksumError) Error() string {
+	return fmt.Sprintf("checksum error (%d != expected %d)", e.got, e.expected)
+}
